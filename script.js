d527fc079f49e2ee2b51943033f72468c5027fae
Towfiqur Rahman <towfiqur.r@tiiqunetwork.onmicrosoft.com>
2025-03-17 11:54:54 +0000

    Changes, running tests

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-import { Octokit } from "octokit";
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+
+import Octokit from "octokit";
 
 const octokit = new Octokit({ });
 
+function updateDynamicText() {
+  document.getElementById('dynamic-text').innerHTML = document.querySelector("input[name='dynamic-text-field']").value;
+  displayDynamicText();
+  return false;
+}
+
+async function displayDynamicText() {
+/*
+const value = async () => {
+  const response = await fetch('https://api.github.com/repos/towfiqur-rahman/arqiva-cloud/actions/variables/dynamic_text', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+  console.log(await response.json());
+};
+value();*/
+  const response = await octokit.request('GET /repos/{owner}/{repo}/actions/variables', {
+    owner: 'towfiqur-rahman',
+    repo: 'arqiva-cloud',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+  console.log(respose);
+  //document.getElementById('dynamic-text').innerHTML = document.querySelector("input[name='dynamic-text-field']").value;
+
+}
